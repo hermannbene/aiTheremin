11670b204ba473372875d6ffa52f828a9eec488d
hermannbene <hermannbene@gmail.com>
2024-03-03 17:33:03 +0100

    initial commit test

--- target/wasm32-unknown-unknown/release/webapp.d
+++ target/wasm32-unknown-unknown/release/webapp.d
@@ -1,1 +1,1 @@
-C:\Users\Benedikt\ Hermann\Documents\aiTheremin\aiTheremin\webapp\target\wasm32-unknown-unknown\release\webapp.wasm: C:\Users\Benedikt\ Hermann\Documents\aiTheremin\aiTheremin\webapp\src\main.rs
+C:\Users\Benedikt\ Hermann\Documents\aiTheremin\aiTheremin\target\wasm32-unknown-unknown\release\webapp.wasm: C:\Users\Benedikt\ Hermann\Documents\aiTheremin\aiTheremin\src\main.rs
